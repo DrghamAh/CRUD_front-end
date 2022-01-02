30bfd12abb778dc1a9b53fd568c1904a17308358
Drgham_Abou_Heassoun <drghamah2@email.com>
2022-01-02 05:10:43 -0600

    Some Modification

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,29 @@
+import axios from "axios";
 import React from "react";
 
 class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories : [],
+      url : 'http://localhost:5000/categories'
+    };
+  }
+
+  componentDidMount() {
+    axios.get(this.state.url).then(response => {
+      this.setState({
+        categories : response.data,
+      });
+    });
+  }
+
+  handleDelete(e) {
+    axios.delete(this.state.url, {
+      _id : e.target.value,
+    });
+  }
 
   render() {
     return (
@@ -8,20 +31,20 @@ class Categories extends React.Component {
         <table className="table">
           <thead>
             <tr>
-              <th>id</th>
               <th>name</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Shoes</td>
-              <td>
-                <button className="btn btn-primary" >Edit</button>
-                <button className="btn btn-danger">Delete</button>
-              </td>
-            </tr>
+            {this.state.categories.map(category => (
+              <tr>
+                <td>{category.name}</td>
+                <td>
+                  <button className="btn btn-primary" >Edit</button>
+                  <button className="btn btn-danger"  >Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </>
